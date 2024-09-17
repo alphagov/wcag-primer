c5afa04740b5d5a83f13dfda4261ca97fd796f4a
beeps <hi+github@berly.kim>
2024-09-17 14:00:52 +0100

    Add internal use banner to all pages

--- config.rb
+++ config.rb
@@ -2,6 +2,9 @@ require 'govuk_tech_docs'
 
 GovukTechDocs.configure(self)
 
+# use custom layout file
+set :layout, 'custom'
+
 # use relative paths for links and sources
 activate :relative_assets
 set :relative_links, true
