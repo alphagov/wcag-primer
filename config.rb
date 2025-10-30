6ab39350f38a81c1e38d79cd85c73051d5230f33
Andrew Hick <18572857+andrewhick@users.noreply.github.com>
2025-10-30 13:04:25 +0000

    Remove banner stating the intended audience

--- config.rb
+++ config.rb
@@ -2,9 +2,6 @@ require 'govuk_tech_docs'
 
 GovukTechDocs.configure(self)
 
-# use custom layout file
-set :layout, 'custom'
-
 # use relative paths for links and sources
 activate :relative_assets
 set :relative_links, true
